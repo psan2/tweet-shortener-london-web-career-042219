ab0b4440c38eda2d41c24fa3f2aca12845a0a2aa
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-31 20:30:57 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -54,3 +54,6 @@ end
 def selective_tweet_shortener(tweet)
   tweet.length > 140 ? word_substituter(tweet) : tweet
 end
+
+def shortened_tweet_truncator(tweet)
+  selective_tweet_shortener(tweet).length > 140 ? "#{word_substituter(tweet)[0..140]}..." : tweet
